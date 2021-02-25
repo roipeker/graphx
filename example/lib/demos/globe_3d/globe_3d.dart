59f4171dc909caf5f0b3b1ba34f89c6c1c886695
rodrigo <roipeker@gmail.com>
2021-02-24 23:35:00 -0300

    - Globe3d

---
+++ example/lib/demos/globe_3d/globe_3d.dart
@@ -0,0 +1,28 @@
+/// roipeker 2020
+///
+import 'package:flutter/material.dart';
+import 'package:graphx/graphx.dart';
+
+import 'scene.dart';
+
+class Globe3dMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          'globe 3d',
+          style: TextStyle(fontSize: 12),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black26,
+      ),
+      body: Center(
+        child: SceneBuilderWidget(
+          builder: () => SceneController(front: Globe3dScene()),
+        ),
+      ),
+    );
+  }
+}
