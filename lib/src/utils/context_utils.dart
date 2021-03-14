b512710970a335f75c2e689dd8c5805f53a20506
Ismail Alam Khan <mdismailalamkhan@gmail.com>
2021-03-14 22:00:17 +0600

    removed the trace when calling ContextUtils.getRenderObjectBounds

--- lib/src/utils/context_utils.dart
+++ lib/src/utils/context_utils.dart
@@ -5,7 +5,6 @@ import '../../graphx.dart';
 abstract class ContextUtils {
   /// todo: validate RenderObject Type.
   static GRect? getRenderObjectBounds(BuildContext context) {
-    trace(context);
     final box = context.findRenderObject() as RenderBox;
     return GRect.fromNative(box.localToGlobal(Offset.zero) & box.size);
   }
