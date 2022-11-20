a9b81b9bb2df354d660cd3d14673badcfebec820
Roi Peker <roipeker@gmail.comr>
2022-11-20 01:18:37 -0300

    Update zoom_pivot_scene.dart
    code simplification.

--- example/lib/demos/zoom_pivot/zoom_pivot_scene.dart
+++ example/lib/demos/zoom_pivot/zoom_pivot_scene.dart
@@ -5,22 +5,31 @@ import 'package:flutter/material.dart';
 import 'package:graphx/graphx.dart';
 
 class ZoomPivotScene extends GSprite {
+
+  late GSprite box;
+  late GShape boxPivot;
+
   @override
   void addedToStage() {
     stage!.color = Colors.white;
 
-    var box = addChild(GSprite())
+    /// Main interaction box (yellow)
+    box = addChild(GSprite())
       ..setPosition(100, 100)
       ..graphics
           .beginFill(Colors.yellow) //
           .drawRect(0, 0, 100, 100) //
           .endFill();
 
-    var boxPivot = box.addChild(GShape())
-      ..graphics.beginFill(Colors.blue).drawCircle(0, 0, 2).endFill();
+    /// Tiny blue dot to identify local (0,0) coordinate in `box`.
+    /// Mo
+    boxPivot = box.addChild(GShape())
+      ..graphics.beginFill(Colors.blue).drawCircle(0, 0, 2).endFill()
+      ..name = 'dot';
 
     /// Some random transformations to box2 to show
     /// accuracy of child interactions.
+    /// A container for [box] with extra transformations.
     var box2 = addChild(GSprite());
     box2.graphics
         .beginFill(Colors.red.withOpacity(.2))
@@ -32,6 +41,9 @@ class ZoomPivotScene extends GSprite {
     /// Comment the following code lines to remove the extra transforms to
     /// the parent container... shows that the pivot is still accurate on nested
     /// transformations.
+    /// WARNING: Mouse interactions on [box] are only valid when the the mouse
+    /// is HOVERING it. So if you have a hard time testing the zoom+pan comment
+    /// this code.
     stage!.onEnterFrame.add((time) {
       var t = stage!.controller.ticker!.currentTime / 3;
       box2.scaleX = 1 + Math.sin(t) * .5;
@@ -43,10 +55,7 @@ class ZoomPivotScene extends GSprite {
     // --------------------
     var dpr = window.devicePixelRatio;
     box.onMouseDown.add((event) {
-      boxPivot.x = box.pivotX = box.mouseX;
-      boxPivot.y = box.pivotY = box.mouseY;
-      box.x = box.parent!.mouseX;
-      box.y = box.parent!.mouseY;
+      centerOnMouse();
 
       stage!.onMouseMove.add((event) {
         box.x = box.parent!.mouseX;
@@ -59,11 +68,18 @@ class ZoomPivotScene extends GSprite {
 
     // Regular mouse support + magic mouse/trackpad on web target.
     box.onMouseScroll.add((event) {
-      boxPivot.x = box.pivotX = box.mouseX;
-      boxPivot.y = box.pivotY = box.mouseY;
-      box.x = box.parent!.mouseX;
-      box.y = box.parent!.mouseY;
+      centerOnMouse();
 
+      // We use `100` cause is the "size" of the [box].
+      // To get 1% of the box size per mouse wheel tick "delta" we divide by 100.
+      // If you change the size of the box, you need to change this value.
+      // If you are on Windows probably [scrollDelta] will not provide a proper
+      // speed value, and you should "adjust" the ratio accordingly to your needs,
+      // on my tests, macOS reports accurate values for mouse wheel "ticks" and
+      // trackpad gestures... this provides an accurate control over the
+      // zoom. On Windows numbers are extremely high, so you need to add a
+      // bigger ratio to get a proper zoom speed (from 20x to 100x more for [scrollDelta]).
+      // Linux was not tested.
       var scaleRatio = (event.scrollDelta.y / dpr) / 100;
 
       /// Shift Key flips Mouse Wheel (horizontal scroll).
@@ -77,21 +93,17 @@ class ZoomPivotScene extends GSprite {
     /// zoom / pan (trackpad and some horizontal mouse wheel on macos)
     var grabScale = 1.0, grabRot = 0.0;
     box.onZoomPan.add((event) {
-      var e = event.rawEvent!.rawEvent;
-      if (e is PointerPanZoomStartEvent) {
+      var rawEvent = event.rawEvent!.rawEvent;
+      if (rawEvent is PointerPanZoomStartEvent) {
         grabScale = box.scale;
         grabRot = box.rotation;
-
-        boxPivot.x = box.pivotX = box.mouseX;
-        boxPivot.y = box.pivotY = box.mouseY;
-        box.x = box.parent!.mouseX;
-        box.y = box.parent!.mouseY;
-      } else if (e is PointerPanZoomUpdateEvent) {
-        if (e.scale == 1 && e.rotation == 0) {
+        centerOnMouse();
+      } else if (rawEvent is PointerPanZoomUpdateEvent) {
+        if (rawEvent.scale == 1 && rawEvent.rotation == 0) {
           // if we do use a mouse or trackpad to scroll horizontally...
           // emulate mouse wheel. (macOS)
-          var scaleRatio = (e.panDelta.dy / dpr) / 100;
-          var rotationRatio = (e.panDelta.dx / dpr) / 100;
+          var scaleRatio = (rawEvent.panDelta.dy / dpr) / 100;
+          var rotationRatio = (rawEvent.panDelta.dx / dpr) / 100;
 
           if (rotationRatio == 0 &&
               GKeyboard.isDown(LogicalKeyboardKey.shift)) {
@@ -101,13 +113,26 @@ class ZoomPivotScene extends GSprite {
           box.scale += scaleRatio;
           box.rotation += rotationRatio;
         } else {
-          box.rotation = grabRot - e.rotation;
-          box.scale = grabScale + (e.scale - 1);
+          box.rotation = grabRot - rawEvent.rotation;
+          box.scale = grabScale + (rawEvent.scale - 1);
         }
 
         /// limit scale to 10% and 1000%.
         box.scale = box.scale.clamp(.1, 10);
       }
     });
+  }
+
+  void centerOnMouse() {
+    // 1 - adjust pivot point (0,0) for [box], and match the inner
+    // [boxPivot] to that pivot (0,0).
+    // Pivot is ALWAYS relative to the parent. Inside a GSprite it has it's own
+    // coordinates, that's why we have to move [boxPivot]
+    boxPivot.x = box.pivotX = box.mouseX;
+    boxPivot.y = box.pivotY = box.mouseY;
+
+    // 2 - match the position of [box] in parent's mouse position coordinates.
+    box.x = box.parent!.mouseX;
+    box.y = box.parent!.mouseY;
   }
 }
